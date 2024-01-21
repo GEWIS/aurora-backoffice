974fb5b87619cd5f42c61abd04b6aac064613037
Yoronex <me@roykakkenberg.nl>
2024-01-21 11:59:30 +0100

    Improve effects controller page layout

--- src/views/Lights/EffectsController.vue
+++ src/views/Lights/EffectsController.vue
@@ -1,29 +1,31 @@
 <template>
-  <div class="flex justify-content-start gap-4">
-    <Card>
-      <template #title>(1) Lights groups</template>
-      <template #content>
-        <div class="flex flex-column gap-2 w-100">
-          <div class="flex flex-row gap-2">
-            <Button severity="secondary" @click="reset">Reset</Button>
-            <Button severity="secondary" @click="selectAll">Select all</Button>
+  <div class="grid">
+    <div class="col-3">
+      <Card class="w-100">
+        <template #title>(1) Lights groups</template>
+        <template #content>
+          <div class="flex flex-column gap-2 w-100">
+            <div class="flex flex-row gap-2">
+              <Button severity="secondary" @click="reset">Reset</Button>
+              <Button severity="secondary" @click="selectAll">Select all</Button>
+            </div>
+            <div v-for="group in activeLightGroups" :key="group.id">
+              <LightsGroupToggleButton
+                :lights-group="group"
+                @click="() => {group.selected = !group.selected; console.log(group.selected);}"
+              />
+            </div>
+            <div v-for="group in inactiveLightGroups" :key="group.id">
+              <LightsGroupToggleButton
+                :lights-group="group"
+                disabled
+              />
+            </div>
           </div>
-          <div v-for="group in activeLightGroups" :key="group.id">
-            <LightsGroupToggleButton
-              :lights-group="group"
-              @click="() => {group.selected = !group.selected; console.log(group.selected);}"
-            />
-          </div>
-          <div v-for="group in inactiveLightGroups" :key="group.id">
-            <LightsGroupToggleButton
-              :lights-group="group"
-              disabled
-            />
-          </div>
-        </div>
-      </template>
-    </Card>
-    <div>
+        </template>
+      </Card>
+    </div>
+    <div class="col-6 flex flex-column gap-3">
       <Card>
         <template #title>
           (2) Create effects
@@ -40,6 +42,23 @@
               {{ e.type }}
             </li>
           </ul>
+        </template>
+      </Card>
+    </div>
+    <div class="col-3">
+      <Card class="w-100">
+        <template #title>
+          Other effects & settings
+        </template>
+        <template #content>
+          Hello!
+        </template>
+      </Card>
+    </div>
+    <div class="col-12">
+      <Card class="w-100">
+        <template #title>
+          Past effects
         </template>
       </Card>
     </div>
@@ -85,4 +104,7 @@ const reset = () => {
 </script>
 
 <style scoped lang="scss">
+.grid > div {
+  padding: 0.5rem;
+}
 </style>
