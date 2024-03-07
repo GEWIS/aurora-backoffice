49c1038a71f2352c5390b77eacdd36e45fa5f435
Gijsdeman <github@deman.dev>
2024-03-07 15:33:11 +0100

    Fix lint conflicts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,8 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'prettier'
   ],
 
   ignorePatterns: [
