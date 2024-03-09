e0467344ec8268e0f75cc00958db6c7db3117af2
Gijsdeman <github@deman.dev>
2024-03-09 15:04:27 +0100

    Remove api from upstream

--- src/api/models/AlcoholTime.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export enum AlcoholTime {
-  _16_30 = '16:30',
-  _14_00 = '14:00'
-}
--- src/api/models/LightsSceneFixtureResponse.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type LightsSceneFixtureResponse = {
-  fixtureId: number;
-  dmxValues: Array<number>;
-};
