11a87295a347901f4a5b56ccb2405ee3a7246924
Roy Kakkenberg <me@roykakkenberg.nl>
2024-04-28 17:09:08 +0200

    Add gobo property to Static Color effect

--- src/components/lights/effects/color/EffectStaticColor.vue
+++ src/components/lights/effects/color/EffectStaticColor.vue
@@ -5,6 +5,7 @@
     @save="handleAddEffect"
   >
     <SelectorLightsColor single-color @colorsUpdated="(c) => (colors = c)" />
+    <Dropdown v-model="gobo" :options="gobos" placeholder="Select a gobo" show-clear />
     <SelectorBoolean
       id="beat-toggle"
       name="Beat Toggle"
@@ -25,16 +26,25 @@
 
 <script setup lang="ts">
 import { useEffectsControllerStore } from '@/stores/effects-controller.store';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import EffectSettingsDialog from '@/components/lights/effects/EffectSettingsDialog.vue';
 import SelectorLightsColor from '@/components/lights/effects/props/SelectorLightsColor.vue';
 import SelectorBoolean from '@/components/lights/effects/props/SelectorBoolean.vue';
 import SelectorRatioSlider from '@/components/lights/effects/props/SelectorRatioSlider.vue';
 import { RgbColor, StaticColorCreateParams } from '@/api';
+import { useSubscriberStore } from '@/stores/subscriber.store';
 
 const store = useEffectsControllerStore();
+const subscriberStore = useSubscriberStore();
+const gobos: string[] = computed(() => {
+  return subscriberStore.lightsGroups
+    .map((g) => g.movingHeadWheels.map((w) => w.gobos))
+    .flat()
+    .flat();
+});
 
 const colors = ref<RgbColor[]>([]);
+const gobo = ref<string>('');
 const beatToggle = ref<boolean>(false);
 const relativeBrightness = ref<number>(1);
 
@@ -43,6 +53,7 @@ const handleAddEffect = () => {
     type: StaticColorCreateParams.type.STATIC_COLOR,
     props: {
       color: colors.value[0],
+      gobo: gobo.value ? gobo.value : undefined,
       beatToggle: beatToggle.value,
       relativeBrightness: relativeBrightness.value
     }
