ae4fad8200d9fa120077ff24a6719f707e0e290d
Gijsdeman <github@deman.dev>
2024-10-14 22:45:08 +0200

    fix(centurion-mode): faulty initialization check

--- src/views/Modes/CenturionModeView.vue
+++ src/views/Modes/CenturionModeView.vue
@@ -1,12 +1,12 @@
 <template>
   <AppContainer title="Centurion" icon="pi-crown">
     <template #header>
-      <div v-if="store.currentTape">
+      <div v-if="currentTape">
         <StopButton :tape="currentTape" />
       </div>
     </template>
     <div class="grid grid-cols-1">
-      <div v-if="store.currentTape">
+      <div v-if="currentTape">
         <CoverPlayButton :tape="currentTape" />
         <divider />
         <CenturionTapeTimeline :tape="currentTape" />
@@ -20,7 +20,7 @@
 
 <script setup lang="ts">
 import { useCenturionStore } from '@/stores/modes/centurion.store';
-import { computed, type ComputedRef, onMounted, onUnmounted, ref } from 'vue';
+import { computed, onMounted, onUnmounted, ref } from 'vue';
 import InitializeCenturion from '@/components/modes/centurion/InitializeCenturion.vue';
 import CoverPlayButton from '@/components/modes/centurion/CoverPlayButton.vue';
 import AppContainer from '@/layout/AppContainer.vue';
@@ -28,21 +28,24 @@ import type { MixTapeResponse } from '@/api';
 import StopButton from '@/components/modes/centurion/StopButton.vue';
 import CenturionTapeTimeline from '@/components/modes/centurion/TapeTimeline.vue';
 
-const store = useCenturionStore();
-store.init();
+const centurionStore = useCenturionStore();
+centurionStore.init();
 
 const onWidthChange = () => (width.value = window.innerWidth);
 onMounted(() => window.addEventListener('resize', onWidthChange));
 onUnmounted(() => {
   window.removeEventListener('resize', onWidthChange);
-  store.destroy();
+  centurionStore.destroy();
 });
 
 let width = ref(window.innerWidth);
-const currentTape: ComputedRef<MixTapeResponse | undefined> = computed<MixTapeResponse | undefined>(
-  () => {
-    const { tapes, currentTape } = store;
-    return tapes?.find((t) => t.name === currentTape?.name);
-  }
-);
+const currentTape = computed<MixTapeResponse | undefined>(() => {
+  const tapes = centurionStore.getTapes;
+  if (!tapes) return undefined;
+
+  const currentTape = centurionStore.getCurrentTape;
+  if (!currentTape) return undefined;
+
+  return tapes?.find((t) => t.name === currentTape?.name);
+});
 </script>
