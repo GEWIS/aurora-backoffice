2b6bc3904c0fa68ab2cdb0f7968b911d95413515
Gijsdeman <github@deman.dev>
2024-10-19 20:09:58 +0200

    refactor(centurion): extract stepper exponent

--- src/components/modes/centurion/InitializeCenturion.vue
+++ src/components/modes/centurion/InitializeCenturion.vue
@@ -1,159 +1,110 @@
 <template>
-  <Stepper value="1">
-    <StepList v-if="!vertical">
-      <Step value="1"> Mixtape </Step>
-      <Step value="2"> Subscribers </Step>
-      <Step value="3"> Confirmation </Step>
-    </StepList>
-    <StepPanels>
-      <!-- Selecting a tape -->
-      <StepperWrapper header="Mixtape" value="1" :wrap="vertical">
-        <StepPanel v-slot="{ activateCallback }" value="1">
-          <AppBox class="flex flex-col gap-2 mt-3 sm:mt-0">
-            <div class="sm:text-lg text-center">Select a mixtape for this centurion</div>
-            <Select
-              class="w-full sm:max-w-80 mx-auto"
-              :model-value="selectedTape"
-              option-group-children="items"
-              option-group-label="label"
-              option-label="name"
-              :options="availableTapes"
-              placeholder="Select tape"
-              :title="selectedTape?.name"
-              @update:model-value="
-                (value: MixTapeResponse) => {
-                  selectedTape = value;
-                }
-              "
-            >
-              <template #optiongroup="slotProps">
-                <p>{{ slotProps.option.label }}</p>
-              </template>
-            </Select>
-            <div class="pt-3 w-full flex justify-center">
-              <Button
-                :disabled="!selectedTape"
-                icon="pi pi-chevron-right"
-                @click="activateCallback('2')"
-              />
-            </div>
-          </AppBox>
-        </StepPanel>
-      </StepperWrapper>
-
-      <!-- Select audio -->
-      <StepperWrapper header="Subscribers" value="2" :wrap="vertical">
-        <StepPanel v-slot="{ activateCallback }" value="2">
-          <AppBox class="flex flex-col gap-2 mt-3 sm:mt-0">
-            <div class="sm:text-lg text-center">Select subscribers for this centurion</div>
-            <MultiSelect
-              class="w-full sm:max-w-80 mx-auto"
-              :max-selected-labels="2"
-              :model-value="selectedAudios"
-              option-label="name"
-              option-value="id"
-              :options="subscriberStore.audios"
-              placeholder="Select audio"
-              :title="
-                subscriberStore.audios
-                  .filter((a) => selectedAudios.includes(a.id))
-                  .map((a) => a.name)
-                  .join(', ')
-              "
-              @update:model-value="(value: number[]) => (selectedAudios = value)"
-            />
-            <MultiSelect
-              class="w-full sm:max-w-80 mx-auto"
-              :max-selected-labels="2"
-              :model-value="selectedScreens"
-              option-label="name"
-              option-value="id"
-              :options="subscriberStore.screens"
-              placeholder="Select screens"
-              :title="
-                subscriberStore.screens
-                  .filter((a) => selectedScreens.includes(a.id))
-                  .map((a) => a.name)
-                  .join(', ')
-              "
-              @update:model-value="(value: number[]) => (selectedScreens = value)"
-            />
-            <MultiSelect
-              class="w-full sm:max-w-80 mx-auto"
-              :max-selected-labels="2"
-              :model-value="selectedLightGroups"
-              option-label="name"
-              option-value="id"
-              :options="subscriberStore.lightsGroups"
-              placeholder="Select lights"
-              :title="
-                subscriberStore.lightsGroups
-                  .filter((a) => selectedLightGroups.includes(a.id))
-                  .map((a) => a.name)
-                  .join(', ')
-              "
-              @update:model-value="(value: number[]) => (selectedLightGroups = value)"
-            />
-
-            <div class="flex pt-3 justify-center gap-5">
-              <Button
-                icon="pi pi-chevron-left"
-                severity="secondary"
-                @click="activateCallback('1')"
-              />
-              <Button
-                :disabled="!selectedAudios.length"
-                icon="pi pi-chevron-right"
-                @click="activateCallback('3')"
-              />
-            </div>
-          </AppBox>
-        </StepPanel>
-      </StepperWrapper>
-
-      <!-- Confirming the selection -->
-      <StepperWrapper header="Confirm" value="3" :wrap="vertical">
-        <StepPanel v-slot="{ activateCallback }" value="3">
-          <AppBox class="flex flex-col gap-2 mt-3 sm:mt-0">
-            <div class="max-w-lg mx-auto">
-              <div class="sm:text-lg text-center mb-3">Confirm centurion initialization</div>
-              <TapeDetails v-if="selectedTape" :tape="selectedTape" />
-              <SubscriberDetails
-                class="mt-3"
-                :selected-audios="selectedAudios"
-                :selected-light-groups="selectedLightGroups"
-                :selected-screens="selectedScreens"
-              />
-              <div class="flex pt-6 justify-center gap-5">
-                <Button
-                  icon="pi pi-chevron-left"
-                  severity="secondary"
-                  @click="activateCallback('2')"
-                />
-                <Button :disabled="!selectedAudios.length" icon="pi pi-check" @click="initialize" />
-              </div>
-            </div>
-          </AppBox>
-        </StepPanel>
-      </StepperWrapper>
-    </StepPanels>
-  </Stepper>
+  <StepperTestTest :steps="initializationSteps">
+    <template #1>
+      <div class="flex flex-col gap-2 mt-3 sm:mt-0">
+        <div class="sm:text-lg text-center">Select a mixtape for this centurion</div>
+        <Select
+          class="w-full sm:max-w-80 mx-auto"
+          :model-value="selectedTape"
+          option-group-children="items"
+          option-group-label="label"
+          option-label="name"
+          :options="availableTapes"
+          placeholder="Select tape"
+          :title="selectedTape?.name"
+          @update:model-value="
+            (value: MixTapeResponse) => {
+              selectedTape = value;
+            }
+          "
+        >
+          <template #optiongroup="slotProps">
+            <p>{{ slotProps.option.label }}</p>
+          </template>
+        </Select>
+      </div>
+    </template>
+    <template #2>
+      <div class="flex flex-col gap-2 mt-3 sm:mt-0">
+        <div class="sm:text-lg text-center">Select subscribers for this centurion</div>
+        <MultiSelect
+          class="w-full sm:max-w-80 mx-auto"
+          :max-selected-labels="2"
+          :model-value="selectedAudios"
+          option-label="name"
+          option-value="id"
+          :options="subscriberStore.audios"
+          placeholder="Select audio"
+          :title="
+            subscriberStore.audios
+              .filter((a) => selectedAudios.includes(a.id))
+              .map((a) => a.name)
+              .join(', ')
+          "
+          @update:model-value="(value: number[]) => (selectedAudios = value)"
+        />
+        <MultiSelect
+          class="w-full sm:max-w-80 mx-auto"
+          :max-selected-labels="2"
+          :model-value="selectedScreens"
+          option-label="name"
+          option-value="id"
+          :options="subscriberStore.screens"
+          placeholder="Select screens"
+          :title="
+            subscriberStore.screens
+              .filter((a) => selectedScreens.includes(a.id))
+              .map((a) => a.name)
+              .join(', ')
+          "
+          @update:model-value="(value: number[]) => (selectedScreens = value)"
+        />
+        <MultiSelect
+          class="w-full sm:max-w-80 mx-auto"
+          :max-selected-labels="2"
+          :model-value="selectedLightGroups"
+          option-label="name"
+          option-value="id"
+          :options="subscriberStore.lightsGroups"
+          placeholder="Select lights"
+          :title="
+            subscriberStore.lightsGroups
+              .filter((a) => selectedLightGroups.includes(a.id))
+              .map((a) => a.name)
+              .join(', ')
+          "
+          @update:model-value="(value: number[]) => (selectedLightGroups = value)"
+        />
+      </div>
+    </template>
+    <template #3>
+      <div class="flex flex-col gap-2 mt-3 sm:mt-0">
+        <div class="max-w-lg mx-auto">
+          <div class="sm:text-lg text-center mb-3 font-semibold">
+            Confirm centurion initialization
+          </div>
+          <TapeDetails v-if="selectedTape" :tape="selectedTape" />
+          <SubscriberDetails
+            class="mt-3"
+            :selected-audios="selectedAudios"
+            :selected-light-groups="selectedLightGroups"
+            :selected-screens="selectedScreens"
+          />
+        </div>
+      </div>
+    </template>
+  </StepperTestTest>
 </template>
 
 <script setup lang="ts">
 import { computed, ref } from 'vue';
-import Stepper from 'primevue/stepper';
-import Step from 'primevue/step';
-import StepPanel from 'primevue/steppanel';
-import StepPanels from 'primevue/steppanels';
-import StepList from 'primevue/steplist';
 import { useSubscriberStore } from '@/stores/subscriber.store';
 import SubscriberDetails from '@/components/modes/centurion/SubscriberDetails.vue';
 import { useCenturionStore } from '@/stores/modes/centurion.store';
-import StepperWrapper from '@/components/prime/StepperWrapper.vue';
 import type { MixTapeResponse } from '@/api';
 import TapeDetails from '@/components/modes/centurion/TapeDetails.vue';
-import AppBox from '@/layout/AppBox.vue';
+import type { StepperStep } from '@/components/prime/StepperWrapper.vue';
+import StepperTestTest from '@/components/prime/StepperWrapper.vue';
 
 interface GroupedTape {
   label: string;
@@ -200,6 +151,12 @@ const availableTapes = computed<GroupedTape[] | undefined>(() => {
   }, [] as GroupedTape[]);
 });
 
+const initializationSteps = computed<StepperStep[]>(() => [
+  { value: 'Mixtape', nextDisabled: selectedTape.value === undefined },
+  { value: 'Subscribers', nextDisabled: selectedAudios.value.length === 0 },
+  { value: 'Confirmation', confirmFunction: initialize }
+]);
+
 // Initialize the centurion
 const initialize = async () => {
   await centurionStore.initializeCenturion(
@@ -210,26 +167,3 @@ const initialize = async () => {
   );
 };
 </script>
-
-<style lang="scss">
-.p-select-option-label,
-.p-select-label {
-  text-overflow: ellipsis;
-  overflow: hidden;
-}
-
-.p-steppanel-content,
-.p-steppanel {
-  padding-left: 0 !important;
-}
-
-.p-stepper-separator {
-  display: none;
-}
-
-@screen sm {
-  .p-stepper-separator {
-    display: flex;
-  }
-}
-</style>
--- src/components/prime/StepperWrapper.vue
+++ src/components/prime/StepperWrapper.vue
@@ -1,18 +1,96 @@
 <template>
-  <StepItem v-if="wrap" :value="value">
-    <Step>{{ header }}</Step>
-    <slot />
-  </StepItem>
-  <slot v-else />
+  <Stepper value="1">
+    <StepList v-if="!vertical">
+      <Step v-for="(step, index) in steps" :key="index" :value="(index + 1).toString()">
+        {{ step.value }}
+      </Step>
+    </StepList>
+
+    <StepPanels>
+      <StepperWrapper
+        v-for="(step, index) in steps"
+        :key="step.value"
+        :header="step.value"
+        :value="(index + 1).toString()"
+        :wrap="vertical"
+      >
+        <StepPanel v-slot="{ activateCallback }" :value="(index + 1).toString()">
+          <AppBox class="mb-4">
+            <slot :name="index + 1" />
+            <div class="flex pt-3 justify-center gap-5">
+              <Button
+                v-if="index > 0"
+                :disabled="steps[index].previousDisabled"
+                icon="pi pi-chevron-left"
+                severity="secondary"
+                @click="activateCallback(index.toString())"
+              />
+              <Button
+                v-if="index < steps.length - 1"
+                :disabled="steps[index].nextDisabled"
+                icon="pi pi-chevron-right"
+                @click="activateCallback((index + 2).toString())"
+              />
+              <Button
+                v-if="index === steps.length - 1"
+                icon="pi pi-check"
+                @click="steps[index].confirmFunction"
+              />
+            </div>
+          </AppBox>
+        </StepPanel>
+      </StepperWrapper>
+    </StepPanels>
+  </Stepper>
 </template>
 
 <script setup lang="ts">
-import StepItem from 'primevue/stepitem';
+import StepPanel from 'primevue/steppanel';
+import StepList from 'primevue/steplist';
+import Stepper from 'primevue/stepper';
 import Step from 'primevue/step';
+import StepPanels from 'primevue/steppanels';
+import { computed } from 'vue';
+import StepperWrapper from '@/components/prime/VerticalStepperWrapper.vue';
+import AppBox from '@/layout/AppBox.vue';
+import { TailwindWidth, useLayoutStore } from '@/stores/layout.store';
+
+interface StepperStep {
+  value: string;
+  nextDisabled?: boolean;
+  previousDisabled?: boolean;
+  confirmFunction?: () => void;
+}
 
 defineProps<{
-  wrap: boolean;
-  value: string;
-  header: string;
+  steps: StepperStep[];
 }>();
+
+const layoutStore = useLayoutStore();
+const vertical = computed(() => layoutStore.getWindowWidth < TailwindWidth.sm);
+
+export type { StepperStep };
 </script>
+
+<style lang="scss">
+.p-select-option-label,
+.p-select-label {
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
+
+.p-steppanel-content,
+.p-steppanel {
+  padding-left: 0 !important;
+}
+
+.p-stepper-separator {
+  display: none;
+}
+
+@screen sm {
+  .p-stepper-separator {
+    display: flex;
+  }
+}
+</style>
