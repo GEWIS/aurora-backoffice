9f03086b377244bbf715460c96c8806b7ab08908
Gijsdeman <github@deman.dev>
2024-10-20 11:59:41 +0200

    docs(time-trail-store): add annotations to store

--- src/stores/modes/time-trail-race.store.ts
+++ src/stores/modes/time-trail-race.store.ts
@@ -22,6 +22,15 @@ import {
 import { useSocketStore } from '@/stores/socket.store';
 import { handleError } from '@/utils/errorHandler';
 
+/**
+ * Time trail store
+ * @param sessionName - The name of the session
+ * @param state - The current state of the session
+ * @param scoreboard - The scoreboard associated with  the session
+ * @param currentPlayer - The current active player
+ * @param loading - Whether the store is loading
+ * @param startTime - The start time of the session
+ */
 interface TimeTrailRaceStore {
   sessionName?: string;
   state?: TimeTrailRaceState;
@@ -42,9 +51,18 @@ export const useTimeTrailRaceStore = defineStore('time-trail-race', {
     },
     disabled(): boolean {
       return (this.sessionName == null || this.state == null) && !this.loading;
-    }
+    },
+    getSessionName: (state) => state.sessionName,
+    getState: (state) => state.state,
+    getScoreboard: (state) => state.scoreboard,
+    getCurrentPlayer: (state) => state.currentPlayer,
+    getLoading: (state) => state.loading,
+    getStartTime: (state) => state.startTime
   },
   actions: {
+    /**
+     * Get data of current initialized session
+     */
     async getTimeTrailMode() {
       this.loading = true;
       try {
@@ -59,6 +77,13 @@ export const useTimeTrailRaceStore = defineStore('time-trail-race', {
       }
       this.loading = false;
     },
+    /**
+     * Initialize a new time trail race session
+     * @param sessionName - The name of the session
+     * @param audioIds - The audio ids to use
+     * @param screenIds - The screen ids to use
+     * @param lightsGroupIds - The lights group ids to use
+     */
     async initializeTimeTrailMode(
       sessionName: string,
       audioIds: number[],
@@ -83,6 +108,10 @@ export const useTimeTrailRaceStore = defineStore('time-trail-race', {
 
       this.loading = false;
     },
+    /**
+     * Register a player for the current session
+     * @param params - The player parameters
+     */
     async registerPlayer(params: RegisterPlayerParams) {
       this.loading = true;
       await raceRegisterPlayer({
@@ -92,12 +121,19 @@ export const useTimeTrailRaceStore = defineStore('time-trail-race', {
         .catch(handleError);
       this.loading = false;
     },
+    /**
+     * Handle a player being registered and update the store
+     * @param event - The event data
+     */
     handleRegisterPlayer(event: RacePlayerRegisteredEvent) {
       this.currentPlayer = event.player;
       this.scoreboard = event.scoreboard;
       this.state = event.state as unknown as TimeTrailRaceState;
       this.sessionName = event.sessionName;
     },
+    /**
+     * Mark the current player as ready
+     */
     async ready() {
       this.loading = true;
       await raceReady()
@@ -105,11 +141,18 @@ export const useTimeTrailRaceStore = defineStore('time-trail-race', {
         .catch(handleError);
       this.loading = false;
     },
+    /**
+     * Handle a player being marked as ready and update the store
+     * @param event - The event data
+     */
     handleReady(event: RacePlayerReadyEvent) {
       this.currentPlayer = event.player;
       this.state = event.state as unknown as TimeTrailRaceState;
       this.sessionName = event.sessionName;
     },
+    /**
+     * Start the current session
+     */
     async start() {
       this.loading = true;
       await raceStart()
@@ -117,12 +160,19 @@ export const useTimeTrailRaceStore = defineStore('time-trail-race', {
         .catch(handleError);
       this.loading = false;
     },
+    /**
+     * Handle a session being started and update the store
+     * @param event - The event data
+     */
     handleStart(event: RaceStartedEvent) {
       this.currentPlayer = event.player;
       this.state = event.state as unknown as TimeTrailRaceState;
       this.sessionName = event.sessionName;
       this.startTime = new Date(event.startTime);
     },
+    /**
+     * Finish the current session
+     */
     async finish() {
       this.loading = true;
       await raceFinish()
@@ -130,12 +180,19 @@ export const useTimeTrailRaceStore = defineStore('time-trail-race', {
         .catch(handleError);
       this.loading = false;
     },
+    /**
+     * Handle a session being finished and update the store
+     * @param event - The event data
+     */
     handleFinish(event: RaceFinishedEvent) {
       this.currentPlayer = event.player;
       this.state = event.state as unknown as TimeTrailRaceState;
       this.sessionName = event.sessionName;
       this.scoreboard = event.scoreboard;
     },
+    /**
+     * Reveal the current session's score
+     */
     async revealScore() {
       this.loading = true;
       await raceRevealScore()
@@ -143,6 +200,10 @@ export const useTimeTrailRaceStore = defineStore('time-trail-race', {
         .catch(handleError);
       this.loading = false;
     },
+    /**
+     * Handle a session's score being revealed and update the store
+     * @param event - The event data
+     */
     handleRevealScore(event: RaceScoreboardEvent) {
       this.currentPlayer = event.player;
       this.state = event.state as unknown as TimeTrailRaceState;
@@ -150,6 +211,9 @@ export const useTimeTrailRaceStore = defineStore('time-trail-race', {
       this.scoreboard = event.scoreboard;
       this.startTime = undefined;
     },
+    /**
+     * Reset the current player - in case something went wrong
+     */
     async resetPlayer() {
       this.loading = true;
       await raceResetPlayer()
@@ -170,14 +234,21 @@ export const useTimeTrailRaceStore = defineStore('time-trail-race', {
       socketStore.backofficeSocket?.on('race-player-finish', this.handleFinish.bind(this));
       socketStore.backofficeSocket?.on('race-scoreboard', this.handleRevealScore.bind(this));
     },
+    /**
+     * Quit the current session
+     */
     async quit() {
       this.loading = true;
-      await disableTimeTrailRacing();
       this.state = undefined;
       this.sessionName = undefined;
       this.scoreboard = [];
+      // TODO; this should be done before methods above; but returns faulty json
+      await disableTimeTrailRacing();
       this.loading = false;
     },
+    /**
+     * Destroy the store - unsubscribe from socket events
+     */
     destroy() {
       const socketStore = useSocketStore();
       socketStore.backofficeSocket?.removeListener(
