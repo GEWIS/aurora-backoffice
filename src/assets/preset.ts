55189c77c9bbb7d099b20f603c092b56cbfa9bbc
Gijsdeman <github@deman.dev>
2024-10-30 10:46:51 +0100

    fix(css): remove unnecessary colors

--- src/assets/preset.ts
+++ src/assets/preset.ts
@@ -1,20 +1,5 @@
 import { definePreset } from '@primevue/themes';
 import Aura from '@primevue/themes/aura';
 
-export const AuraPreset = definePreset(Aura, {
-  semantic: {
-    primary: {
-      50: '{rose.50}',
-      100: '{rose.100}',
-      200: '{rose.200}',
-      300: '{rose.300}',
-      400: '{rose.400}',
-      500: '{rose.500}',
-      600: '{rose.600}',
-      700: '{rose.700}',
-      800: '{rose.800}',
-      900: '{rose.900}',
-      950: '{rose.950}'
-    }
-  }
-});
+// Can be used to extend the Aura theme
+export const AuraPreset = definePreset(Aura, {});
