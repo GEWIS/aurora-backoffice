911586d85ce30168aa26c8eb13bdd451e71984ed
Wanderer <tycholouws@gmail.com>
2026-06-11 13:52:52 +0200

    feat: add confirmation popup for static show button

--- src/components/poster/StaticPosterButtonShow.vue
+++ src/components/poster/StaticPosterButtonShow.vue
@@ -5,16 +5,20 @@
     icon="pi pi-eye"
     :loading="loading"
     severity="success"
-    @click="handleShow"
+    @click="confirmShow"
   />
+
+  <ConfirmPopup />
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import { useConfirm } from 'primevue/useconfirm';
 import type { PosterResponse } from '@/api';
 import { usePosterStore } from '@/stores/poster/poster.store';
 
 const store = usePosterStore();
+const confirm = useConfirm();
 
 const loading = ref<boolean>(false);
 const props = withDefaults(
@@ -29,6 +33,18 @@ const handleShow = () => {
   loading.value = true;
   void store.setStaticActivePoster(props.poster.id).finally(() => {
     loading.value = false;
+  });
+};
+
+const confirmShow = () => {
+  confirm.require({
+    message: `Set ALL static poster screens to this poster?`,
+    header: 'Confirm',
+    acceptLabel: 'Confirm',
+    rejectLabel: 'Cancel',
+    accept: () => {
+      handleShow();
+    },
   });
 };
 </script>
